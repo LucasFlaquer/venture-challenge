f2537d5656b5f02c25a0b4c8e8a648d74d1da828
Lucas Flaquer <lucas.flaquer@vm2.com.br>
2019-09-09 09:27:00 -0300

    engines

--- src/components/EngineForm.vue
+++ src/components/EngineForm.vue
@@ -3,7 +3,7 @@
     <b-row>
       <b-col cols="6">
         <figure>
-          <img src="" alt="">
+          <img :src="require(`@/assets/Engine${checked}.jpg`)" alt="">
         </figure>
       </b-col>
       <b-col cols="6">
@@ -12,7 +12,8 @@
             <label v-for="engine in engines" 
                    :key="engine.id"
                    :for="engine.id" class="engine--label" 
-                   :class="checked === engine.id ? 'active': ''">
+                   :class="checked === engine.id ? 'active': ''"
+                   @click="changeImage">  
               <p>{{engine.kwh}}<span>{{engine.type}}</span></p>
               <p>{{engine.kwh}}<span>kwp</span></p>
               <p>{{engine.range}}<span>miles range</span></p>
@@ -29,12 +30,21 @@
 export default {
   data() {
     return {
-      checked: 1
+      checked: 1,
+      imageUrl: require(`@/assets/Engine${checked}.jpg`)
     }
   },
   props: {
     engines: Array,
-
+  },
+  methods: {
+    changeImage() {
+      if(this.checked > 1) {
+        this.imageUrl = require(`@/assets/Engine${2}.jpg`)
+      }
+      else
+        this.imageUrl = require(`@/assets/Engine${checked}.jpg`)
+    }
   }
 }
 </script>
